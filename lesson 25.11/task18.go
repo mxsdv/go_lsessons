c33840aa08308426431213e69adee0d60a16ce79
mxsdv <9633270025m@gmail.com>
2025-12-01 17:32:10 +0300

    task18_ansver

--- lesson 25.11/task18.go
+++ lesson 25.11/task18.go
@@ -1,15 +1,35 @@
 //todo: Заданы массивы
 
 //Даны читатели книг
-//var readers_books = [...] string {'id3', 'id5', 'id9', 'id8', 'id2', 'id1' }
+//var readers_books = [...] string {"id3", "id5", "id9", "id8", "id2", "id1" }
 
 //Даны читатели газет
-//var readers_magazines = [...] string { 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+//var readers_magazines = [...] string { "id8", "id2", "id1", "id4", "id6", "id7", "id10"}
 
 //Найти пользователей кто читает и книги и газеты
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	readers_books := []string{"id3", "id5", "id9", "id8", "id2", "id1"}
+	readers_magazines := []string{"id8", "id2", "id1", "id4", "id6", "id7", "id10"}
+
+	// карта должна быть map[string]bool
+	m := make(map[string]bool)
+
+	for _, v := range readers_books {
+		m[v] = true
+	}
+
+	var intersection []string
+
+	for _, v := range readers_magazines {
+		if m[v] {
+			intersection = append(intersection, v)
+		}
+	}
+
+	fmt.Println("Читают и книги и журналы", intersection)
 }
